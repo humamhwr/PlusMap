b430468735403bf4d3c3d06aab3dd935f45346e6
Ben Stix <ben@stix.site>
2020-02-01 14:31:41 -0500

    added basic server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
@@ -5,10 +6,31 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
 
-client.messages
-  .create({
-     body: 'want coffee!!',
-     from: '+16473706627',
-     to: '+15144330161'
-   })
-  .then(message => console.log(message.sid));
+
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+const app = express();
+
+app.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+  
+  twiml.message('The Robots are coming! Head for the hills!');
+  
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+    console.log('Express server listening on port 1337');
+});
+
+
+
+
+// client.messages
+//   .create({
+//      body: 'want coffee!!',
+//      from: '+16473706627',
+//      to: '+14385808069'
+//    })
+//   .then(message => console.log(message.sid));
