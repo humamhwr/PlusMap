956864973ba292ea120d7f9b13f90751fbef282c
Ben Stix <ben@stix.site>
2020-02-01 18:22:02 -0500

    made functions

--- server.js
+++ server.js
@@ -5,46 +5,51 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
 const app = express();
 const api = require("@what3words/api");
-            
-api.setOptions({ key: process.env.WHAT3WORDS });
-
-api.convertTo3wa({lat:51.520847, lng:-0.195521})
-  .then(data => console.log(data));
-
-api.autosuggest("freshen.overlook.clo", { 
-    nFocusResults: 1,
-    clipToCountry: ["FR"],
-    focus: {lat:48.856618, lng:2.3522411},
-    nResults: 1
-  })
-  .then(function(response) {
-    var words = response.suggestions[0].words;
-    console.log("Top 3 word address match: " + words);
-
-    api.convertToCoordinates(words).then(function(response) {
-      console.log("WGS84 Coordinates: " + 
-        response.coordinates.lat + ', ' + response.coordinates.lng);
-      console.log("Nearest Place: " + response.nearestPlace);
-    });
-  })
-  .catch(function(error) {
-    console.log("[code]", error.code);
-    console.log("[message]", error.message);
-  });
-
-
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 
+api.setOptions({ key: process.env.WHAT3WORDS });
+
+function convertTo3WA (lat, long) {
+    return new Promise((resolve, reject) => {
+        api.convertTo3wa({ lat: lat, lng: long }).then((response) => {
+            resolve(response.words);
+        });
+    })
+    
+}
+
+
+function convertToCoords(words) {
+    return new Promise((resolve, reject) => {
+        api.convertToCoordinates(words).then((response) => {
+            resolve({lat: response.coordinates.lat, lng: response.coordinates.lng});
+        })
+    })
+}
+
+wa = convertTo3WA(51.520847, -0.195521);
+coords = convertToCoords("embedded.fizzled.trial");
+
+wa.then((r) => {
+   console.log(r); 
+});
+
+coords.then((r) => {
+    console.log(r);
+})
+
+
+console.log(wa, coords);
+
+
 app.post('/sms', (req, res) => {
-  const twiml = new MessagingResponse();
-  console.log(req.body.Body);
-  
-  twiml.message('The Robots are coming! Head for the hills!');
-  
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+    const twiml = new MessagingResponse();
+    console.log(req.body.Body);
+
+    twiml.message('The Robots are coming! Head for the hills!');
+
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(twiml.toString());
 });
 
 http.createServer(app).listen(1337, () => {
