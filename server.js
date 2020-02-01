77a86ffecffbc510935708bb0a5465d7fde487d2
Ben Stix <ben@stix.site>
2020-02-01 14:43:51 -0500

    fix

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
+// require('dotenv').config();
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 
-const client = require('twilio')(accountSid, authToken);
+// const client = require('twilio')(accountSid, authToken);
 
 
-
+const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 const app = express();
